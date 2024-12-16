d7f0f04f4234731182060e17f80680b0a1467513
Henrique-Alves12 <henriquesilvaxdz6@gmail.com>
2024-12-16 13:39:57 -0300

    primeiro commit

---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,11 @@
+
+
+function Home () {
+    return (
+        <div>
+            <h1>Jesus te ama</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/containers/Home/styles.js
@@ -0,0 +1,5 @@
+import styled from 'style-components'
+
+export const Title = styled.h1 `
+    color: red;
+`
